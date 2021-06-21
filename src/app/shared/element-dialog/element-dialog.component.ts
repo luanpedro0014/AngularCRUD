a005c4d555817980a4da192774c0dc8e4e1a778c
luanpedro0014 <luanpedro0014@gmail.com>
2021-06-21 13:20:05 -0300

    1 Commit Crud

---
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -0,0 +1,29 @@
+import { Component,Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { PeriodicElement } from 'src/app/views/home/home.component';
+
+@Component({
+  selector: 'app-element-dialog',
+  templateUrl: './element-dialog.component.html',
+  styleUrls: ['./element-dialog.component.scss']
+})
+export class ElementDialogComponent implements OnInit {
+  element!:PeriodicElement;
+
+ constructor(
+
+  @Inject(MAT_DIALOG_DATA)
+   public data: PeriodicElement,
+    public dialogRef: MatDialogRef<ElementDialogComponent>,)
+     {}
+
+  ngOnInit(): void
+   {
+   }
+
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+}
